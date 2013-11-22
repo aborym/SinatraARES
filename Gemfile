fdb034f236d2f178d0c48715556aa1875de14c45
Filip Ambrož <aborym@aborym.cz>
2013-11-22 20:30:41 +0100

    ADD counters

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'ares_cz'
 gem 'yajl-ruby'
 gem 'thin'
 
+gem 'redis'
+
 group :development do 
 	gem 'shotgun'
 end
--- ares_web.rb
+++ ares_web.rb
@@ -1,20 +1,29 @@
 require 'sinatra'
 require 'yajl'
 require 'ares_cz'
+require 'redis'
+require 'uri'
+
+ENV["REDISTOGO_URL"] = 'redis://127.0.0.1:6379' if ENV['RACK_ENV'] == 'development'
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 get '/' do
-
 	content_type :json
 	halt 403 unless params['token'] == "BAF"
 
 	if params['ico'] && params['ico'].size == 8
+		REDIS.incr('pristupy')
 		ares = Ares.find(ico: params['ico'])
 		if ares.found?
+			REDIS.incr('nalezeno')
 			data = {found: ares.answer}
 			code = 200
 		else
-		data = { error: "IC not found"} 
-		code = 404
+			REDIS.incr('nenalezeno')
+			data = { error: "IC not found"} 
+			code = 404
 		end
 	else 
 		data = { error: "IC is required"} 
@@ -25,6 +34,16 @@ get '/' do
 
 end
 
-get '/is_alive'
+get '/is_alive' do
 	"Yeah online :D"
+end
+
+get '/stats' do
+	content_type :json
+	data = {
+			celkem: REDIS.get('pristupy'),
+			nalezeno: REDIS.get('nalezeno'),
+			nenalezeno: REDIS.get('nenalezeno'),
+		}
+	halt 200, Yajl::Encoder.encode(data)
 end
